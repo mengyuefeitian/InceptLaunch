9b8d1f3e0bff9a00fcb44622be6dd029e3f0474a
小安 <mengyuefeitian@gmail.com>
2026-07-25 22:03:58 +0800

    chore: bump version to 1.5.5

--- script/build_and_run.sh
+++ script/build_and_run.sh
@@ -57,9 +57,9 @@ cat >"$INFO_PLIST" <<PLIST
   <key>CFBundleName</key>
   <string>$APP_NAME</string>
   <key>CFBundleShortVersionString</key>
-  <string>1.5.2</string>
+  <string>1.5.5</string>
   <key>CFBundleVersion</key>
-  <string>1.5.2</string>
+  <string>1.5.5</string>
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>LSMinimumSystemVersion</key>
